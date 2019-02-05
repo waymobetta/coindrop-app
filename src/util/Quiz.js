653b78aeaa6dd89e57f4e389b62892c8f9bb77c4
waymobetta <jbroethke@gmail.com>
2019-02-05 15:15:02 -0800

    remove unneeded method

--- src/util/Quiz.js
+++ src/util/Quiz.js
@@ -1,48 +1,6 @@
 import { baseURL } from './api'
 
 const Quiz = {
-  async postAnswers (quizObj, JwtToken) {
-    const payload = {
-      'title': quizObj.title,
-      'auth_user_id': quizObj.userID,
-      'quiz_info': {
-        'quiz_data': [
-          {
-            'question': '',
-            'answer': quizObj.qOneAns
-          },
-          {
-            'question': '',
-            'answer': quizObj.qTwoAns
-          },
-          {
-            'question': '',
-            'answer': quizObj.qThreeAns
-          },
-          {
-            'question': '',
-            'answer': quizObj.qFourAns
-          },
-          {
-            'question': '',
-            'answer': quizObj.qFiveAns
-          }
-        ]
-      }
-    }
-    return fetch(`${baseURL}/postresults`, {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json',
-        'Authorization': 'Bearer ' + JwtToken
-      },
-      body: JSON.stringify(payload)
-    }).then(res => {
-      return res.json()
-    }).then(jsonResponse => {
-      return jsonResponse
-    })
-  },
   async getResults (newQuizObj, JwtToken) {
     const payload = {
       'title': newQuizObj.title,
