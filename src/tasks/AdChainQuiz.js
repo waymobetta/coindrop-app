08ba641f5af1bb1c1d5703b1f91ff2622f2d7341
waymobetta <jbroethke@gmail.com>
2019-02-11 19:00:34 -0800

    embed quiz component, add button, cleanup page layout

--- src/tasks/AdChainQuiz.js
+++ src/tasks/AdChainQuiz.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { Auth } from 'aws-amplify'
 import './AdChainQuiz.css'
+import LoaderButton from '../components/LoaderButton'
+import * as typeformEmbed from '@typeform/embed'
 
 export default class AdChainArchaeologist extends Component {
   constructor (props) {
@@ -10,6 +12,7 @@ export default class AdChainArchaeologist extends Component {
       userID: '',
       userName: '',
       wallet: '',
+      isCompleted: false,
       token: ''
     }
   }
@@ -24,7 +27,7 @@ export default class AdChainArchaeologist extends Component {
       this.setState({
         userID: currentUser.signInUserSession.accessToken.payload.username,
         userName: currentUser.attributes.email,
-        wallet: '0x123', // placeholder
+        wallet: '0x46f85C845BE7Bd2062b13d60139d952a6cFBC3e8', // placeholder
         token: jwt
       })
     } catch (e) {
@@ -32,12 +35,27 @@ export default class AdChainArchaeologist extends Component {
     }
   }
 
-  render () {
-    const userName = this.state.userName
-    const userNameShort = userName.split('@')[0]
-    const userWallet = this.state.wallet
-    const surveyUrl = `https://coindrop.typeform.com/to/mDDkkK?name=${userNameShort}&walletaddress=${userWallet}`
+  launchTypeformPopUp (event) {
+    try {
+      const userName = this.state.userName
+      const userNameShort = userName.split('@')[0]
+      const userWallet = this.state.wallet
+      const surveyUrl = `https://coindrop.typeform.com/to/mDDkkK?name=${userNameShort}&walletaddress=${userWallet}`
+      const reference = typeformEmbed.makePopup(
+        surveyUrl, {
+          mode: 'popup',
+          hideHeaders: true,
+          hideFooter: true,
+          onSubmit: () => alert('ADT [token] incoming!!') // placeholder
+        }
+      )
+      reference.open()
+    } catch (err) {
+      console.error(err.message)
+    }
+  }
 
+  render () {
     return (
       <div
         align='center'
@@ -62,14 +80,18 @@ export default class AdChainArchaeologist extends Component {
           allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
           allowFullScreen />
         <div
-          className='typeform-widget'
-          data-url={surveyUrl}
-          data-transparency='50'
-          data-hide-headers='true'
-          data-hide-footer='true' />
-        {
-          (function () { var js; var q; var d = document; var gi = d.getElementById; var ce = d.createElement; var gt = d.getElementsByTagName; var id = 'typef_orm'; var b = 'https://embed.typeform.com/'; if (!gi.call(d, id)) { js = ce.call(d, 'script'); js.id = id; js.src = b + 'embed.js'; q = gt.call(d, 'script')[0]; q.parentNode.insertBefore(js, q) } })()
-        }
+          className='typeform'>
+          <LoaderButton
+            block
+            className='button--cd btn btn-outline-primary'
+            outline
+            color='primary'
+            onClick={event => this.launchTypeformPopUp(event)}
+            type='submit'
+            disabled={this.state.isCompleted}
+            text="let's go!"
+          />
+        </div>
       </div>
     )
   }
